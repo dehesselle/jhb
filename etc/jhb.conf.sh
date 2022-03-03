dd3c47938c0e9db2df37c67e778ee3273f56ff9c
René de Hesselle <dehesselle@web.de>
2022-03-03 01:21:03 +0100

    Move configuration inclusion to jhb.conf.sh

--- etc/jhb.conf.sh
+++ etc/jhb.conf.sh
@@ -1,28 +1,29 @@
-#!/usr/bin/env bash
-#
-# SPDX-FileCopyrightText: 2021 René de Hesselle <dehesselle@web.de>
+# SPDX-FileCopyrightText: 2022 René de Hesselle <dehesselle@web.de>
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 ### description ################################################################
 
-# This script is a wrapper around the jhbuild binary to run it in our
-# configured environment (etc/jhb.conf).
+# This is a convenience wrapper to source all individual configuration files.
 
 ### shellcheck #################################################################
 
-# Nothing here.
+# shellcheck shell=bash # no shebang as this file is intended to be sourced
 
 ### includes ###################################################################
 
-#---------------------------------------------------------- source configuration
-
-source "$(dirname "${BASH_SOURCE[0]}")"/../../etc/jhb.conf.sh
+# shellcheck disable=SC1090 # can't point to a single source here
+for CONFIG in $(\
+    "$(dirname "${BASH_SOURCE[0]}")"/../usr/bin/run-parts list \
+        "$(dirname "${BASH_SOURCE[0]}")"/jhb.conf/'*.sh' \
+    ); do
+  source "$CONFIG"
+done
+unset CONFIG
 
 ### variables ##################################################################
 
-# shellcheck disable=SC2034 # export not desired
-PYTHON=$BIN_DIR/python3   # required for libxml2
+# Nothing here.
 
 ### functions ##################################################################
 
@@ -30,4 +31,4 @@ PYTHON=$BIN_DIR/python3   # required for libxml2
 
 ### main #######################################################################
 
-"$USR_DIR"/bin/jhbuild "$@"
+# Nothing here.
