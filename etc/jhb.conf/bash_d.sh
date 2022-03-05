28202da3def132a1a77227aec884aaf1cb93ccbc
René de Hesselle <dehesselle@web.de>
2022-03-05 15:59:45 +0100

    Make bash_d_include available in jhb.conf

---
+++ etc/jhb.conf/bash_d.sh
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2022 René de Hesselle <dehesselle@web.de>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+### description ################################################################
+
+# Enable bash_d usage by providing bash_d_include function.
+
+### shellcheck #################################################################
+
+# shellcheck shell=bash # no shebang as this file is intended to be sourced
+
+### dependencies ###############################################################
+
+# shellcheck source=../../usr/src/bash_d/bash_d.sh
+source "$(dirname "${BASH_SOURCE[0]}")"/../../usr/src/bash_d/bash_d.sh
+
+### variables ##################################################################
+
+# Nothing here.
+
+### functions ##################################################################
+
+# Nothing here.
+
+### main #######################################################################
+
+# Nothing here.
