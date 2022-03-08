400d41f26d81f175a138a5ccaea008674d288df1
René de Hesselle <dehesselle@web.de>
2022-03-08 22:18:21 +0100

    Disable ccache for arm64

--- usr/src/jhb/ccache.sh
+++ usr/src/jhb/ccache.sh
@@ -21,6 +21,7 @@ export CCACHE_DIR=${CCACHE_DIR:-$WRK_DIR/ccache}
 # https://ccache.dev
 # https://github.com/ccache/ccache
 # https://github.com/dehesselle/ccache_macos
+# TODO: arm64 support, see below
 CCACHE_VER=4.5.1r1
 CCACHE_URL=https://github.com/dehesselle/ccache_macos/releases/download/\
 v$CCACHE_VER/ccache_v$CCACHE_VER.tar.xz
@@ -41,6 +42,12 @@ EOF
 
 function ccache_install
 {
+  if [ "$(uname -m)" = "arm64" ]; then
+    # TODO: arm64 support, see above
+    echo_w "ccache for arm64 not implemented yet"
+    return 0
+  fi
+
   curl -L $CCACHE_URL | tar -C "$USR_DIR"/bin --exclude="ccache.sha256" -xJ
 
   for compiler in clang clang++ gcc g++; do
