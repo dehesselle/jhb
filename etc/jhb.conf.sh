e6a693e0bbca31f1d7c6a06bc9aebc4fcdc88b31
René de Hesselle <dehesselle@web.de>
2022-08-20 16:46:41 +0200

    Enable bootstrap to install custom config

--- etc/jhb.conf.sh
+++ etc/jhb.conf.sh
@@ -5,28 +5,40 @@
 ### description ################################################################
 
 # This is a convenience wrapper to source all individual configuration files.
+# It also takes care of placing a custom configuration file in the appropriate
+# location.
 
 ### shellcheck #################################################################
 
 # shellcheck shell=bash # no shebang as this file is intended to be sourced
 
-### includes ###################################################################
+### dependencies ###############################################################
+
+SELF_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1; pwd)
+
+CUSTOM_CONFIG=$SELF_DIR/jhb-custom.conf.sh
+# copy a custom configuration file to the appropriate place
+if [[ $1 == *.conf.sh ]] && [ -f "$1" ]; then
+  cp "$1" "$CUSTOM_CONFIG"
+fi
 
 # source a custom configuration file if present
-if [ -f "$(dirname "${BASH_SOURCE[0]}")"/jhb-custom.conf.sh ]; then
-  # shellcheck disable=SC1091 # file is optional
-  source "$(dirname "${BASH_SOURCE[0]}")"/jhb-custom.conf.sh
+if [ -f "$CUSTOM_CONFIG" ]; then
+  # shellcheck disable=SC1090 # file is optional
+  source "$CUSTOM_CONFIG"
 fi
+unset CUSTOM_CONFIG
 
 # source items from jhb.conf directory
 for CONFIG_ITEM in $(\
-    "$(dirname "${BASH_SOURCE[0]}")"/../usr/bin/run-parts list \
-        "$(dirname "${BASH_SOURCE[0]}")"/jhb.conf/'*.sh' \
+      "$SELF_DIR"/../usr/bin/run-parts list "$SELF_DIR"/jhb.conf/'*.sh' \
     ); do
   # shellcheck disable=SC1090 # can't point to a single source here
   source "$CONFIG_ITEM"
 done
 unset CONFIG_ITEM
+
+unset SELF_DIR
 
 ### variables ##################################################################
 
