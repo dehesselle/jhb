5848a7ce239e9e1e5521188755e712f35804336f
René de Hesselle <dehesselle@web.de>
2022-08-25 23:51:23 +0200

    Fix RELEASE_URLS array default value

--- etc/jhb.conf/release.sh
+++ etc/jhb.conf/release.sh
@@ -16,12 +16,13 @@
 RELEASE_ARCHIVE=${RELEASE_ARCHIVE:-$(basename "$VER_DIR")_$(uname -m).tar.xz}
 
 # GitHub and GitLab
-RELEASE_URLS=${RELEASE_URLS:-(
+# shellcheck disable=2206 # we need expansion for the array to work
+RELEASE_URLS=(${RELEASE_URLS:-
   "https://github.com/dehesselle/jhb/releases/download/\
 v$VERSION/$RELEASE_ARCHIVE"
   "https://gitlab.com/api/v4/projects/35965804/packages/generic/jhb/\
 $VERSION/$RELEASE_ARCHIVE"
-)}
+})
 
 RELEASE_OVERLAY=overlay
 
