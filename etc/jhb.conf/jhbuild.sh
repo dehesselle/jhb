07e263aeb265a3c7ae749073135bcb2c538e0793
René de Hesselle <dehesselle@web.de>
2022-12-11 20:23:17 +0100

    Update certifi to 2022.12.7

--- etc/jhb.conf/jhbuild.sh
+++ etc/jhb.conf/jhbuild.sh
@@ -20,7 +20,7 @@ export JHBUILDRC=$ETC_DIR/jhbuildrc
 export JHBUILDRC_CUSTOM=$JHBUILDRC-custom
 
 JHBUILD_REQUIREMENTS="\
-  certifi==2022.9.24\
+  certifi==2022.12.7\
   meson==0.59.2\
   ninja==1.10.2.3\
 "
