15f1615a831eb5c5d8b7242a24c87a0b7a77c04a
René de Hesselle <dehesselle@web.de>
2023-09-18 21:17:50 +0200

    Allow using external ccache
    This accounts for both binary and configuration.

--- etc/jhb.conf.d/ccache.sh
+++ etc/jhb.conf.d/ccache.sh
@@ -14,7 +14,7 @@
 
 export CCACHE_DIR=${CCACHE_DIR:-$WRK_DIR/ccache}
 
-CCACHE_MAXSIZE=${CCACHE_MAXSIZE:-3.5G}
+CCACHE_BIN=${CCACHE_BIN:-ccache}
 
 # https://ccache.dev
 # https://github.com/ccache/ccache
@@ -28,23 +28,26 @@ ccache_macos/$CCACHE_VER/ccache_$(uname -m).tar.xz"
 
 function ccache_configure
 {
-  mkdir -p "$CCACHE_DIR"
+  # Create directory and configuration if it doesn't exist.
+  if [ ! -d "$CCACHE_DIR" ]; then
+    mkdir -p "$CCACHE_DIR"
 
-  cat <<EOF >"$CCACHE_DIR/ccache.conf"
+    cat <<EOF >"$CCACHE_DIR/ccache.conf"
 base_dir = $WRK_DIR
 hash_dir = false
-max_size = $CCACHE_MAXSIZE
+max_size = 3.0G
 temporary_dir = $CCACHE_DIR/tmp
 EOF
+  fi
+
+  for compiler in clang clang++ gcc g++; do
+    ln -sf "$CCACHE_BIN" "$USR_DIR"/bin/$compiler
+  done
 }
 
 function ccache_install
 {
   curl -L "$CCACHE_URL" | tar -C "$USR_DIR"/bin --exclude="ccache.sha256" -xJ
-
-  for compiler in clang clang++ gcc g++; do
-    ln -sf ccache "$USR_DIR"/bin/$compiler
-  done
 }
 
 ### main #######################################################################
