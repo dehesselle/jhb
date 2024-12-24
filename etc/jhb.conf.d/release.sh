66b0ccad9bdf8896643b84515ffc5319103abeb2
René de Hesselle <dehesselle@web.de>
2024-12-24 14:32:20 +0100

    Fix comment about canonical release source

--- etc/jhb.conf.d/release.sh
+++ etc/jhb.conf.d/release.sh
@@ -15,7 +15,7 @@
 
 RELEASE_ARCHIVE=${RELEASE_ARCHIVE:-$(basename "$VER_DIR")_$(uname -m).tar.xz}
 
-# GitHub and GitLab
+# The canonical source for releases is GitLab.
 # shellcheck disable=2206 # we need expansion for the array to work
 RELEASE_URLS=(${RELEASE_URLS[@]:-
   "https://gitlab.com/api/v4/projects/35965804/packages/generic/jhb/\
