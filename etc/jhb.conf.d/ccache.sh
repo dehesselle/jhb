4c2b0108df41ca48fe0de46b65948b2c2ef3a78f
René de Hesselle <dehesselle@web.de>
2025-05-06 00:02:37 +0200

    Update ccache to 4.11.3

--- etc/jhb.conf.d/ccache.sh
+++ etc/jhb.conf.d/ccache.sh
@@ -18,7 +18,7 @@ CCACHE_BIN=${CCACHE_BIN:-ccache}
 
 # https://ccache.dev
 # https://github.com/ccache/ccache
-CCACHE_VER=${CCACHE_VER:-4.11.2}
+CCACHE_VER=${CCACHE_VER:-4.11.3}
 CCACHE_URL="https://github.com/ccache/ccache/releases/download/v$CCACHE_VER/\
 ccache-$CCACHE_VER-darwin.tar.gz"
 
